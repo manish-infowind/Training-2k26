f7a9997f88a85fbaa69c41252c6ef5182001f9bd
mahimaiwdev-hash <mahima.iwdev@gmail.com>
2026-03-18 18:39:13 +0530

    Add files via upload

---
+++ third.ts
@@ -0,0 +1,47 @@
+// let value: any = "hello";
+// let len = (value as string).length;
+// console.log(len);
+// let data: [string, number] = ["Rahul", 21];
+// console.log(data[1]);
+// enum Role {
+//   ADMIN,
+//   USER
+// }
+// let result: any;
+
+// result = 1;
+// console.log(result);
+
+// result = 'Hello';
+// console.log(result);
+
+// result = [1, 2, 3];
+// const total = result.reduce((a: number, b: number) => a + b, 0);
+// console.log(total);
+// function Log(target, key, descriptor) {
+//   const original = descriptor.value;
+
+//   descriptor.value = function (...args) {
+//     console.log("Method call hua:", key);
+//     return original.apply(this, args);
+//   };
+// }
+// let x: string | number;
+
+// x = "Hi";
+// console.log(x);   
+
+// x = 10;
+// console.log(x);   
+// type A = { name: string };
+// type B = { age: number };
+
+// let user: A & B = { name: "mahiee", age: 22};
+
+// console.log(user);
+// enum Color {
+//   RED,
+//   BLUE
+// }
+
+// console.log(Color.RED);
